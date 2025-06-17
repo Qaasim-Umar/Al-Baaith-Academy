db698b755ffc5ab6c2be6291494e83b600048c68
Qaasim Uumar <omayr010@gmail.com>
2025-06-17 06:03:25 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+      <div className='text-5xl flex items-center justify-center h-screen '> HI @ prudent  </div>
+    </>
+  )
+}
+
+export default App
