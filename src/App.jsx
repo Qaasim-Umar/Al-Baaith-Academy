06a7b5da67f400571f66af6e4b910b04f126c493
Qaasim Uumar <omayr010@gmail.com>
2025-06-17 10:33:36 -0700

    feat: addec routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-
-
   return (
     <>
-      <div className='text-5xl flex items-center justify-center h-screen '> HI @ prudent  </div>
+
+     {/* Navigation */}
+      <nav className=" space-x-5 p-10 text-3xl  ">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+
+        {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className=' items-center justify-center text-9xl text-red-900 h-screen flex'> Hi at Prudent </div>
+    </>
+  )
+}
+
+export default Home
